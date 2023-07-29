c93724a3dfb0f57bc9b3dd54991deda4bd69a652
Jmsabanes07 <jmsabanes07@gmail.com>
2023-07-29 20:16:41 -0300

    added sql server compatibility

--- libs/dbconnsqlserver.js
+++ libs/dbconnsqlserver.js
@@ -7,29 +7,34 @@ const config = {
   server: 'nombre_servidor', // Puede ser una dirección IP o un nombre de servidor
   database: 'nombre_base_de_datos',
   options: {
-    encrypt: true, // Establecer a true si estás utilizando una conexión segura (por ejemplo, con Azure)
+    encrypt: false, // Establecer a true si estás utilizando una conexión segura (por ejemplo, con Azure)
   },
 }
 
 // Función para realizar una consulta a la base de datos
-async function execute(query) {
-  try {
-    // Establecer una conexión a la base de datos
-    await sql.connect(config)
+const execute = query =>
+  new Promise(async (resolve, reject) => {
+    try {
+      // Establecer una conexión a la base de datos
+      await sql.connect(config)
 
-    // Ejecutar la consulta
-    const result = await sql.query(query)
+      // Ejecutar la consulta
+      const result = await sql.query(query)
 
-    // Imprimir los resultados
-    console.log(result.recordset)
+      // Imprimir los resultados
+      console.log(result.recordset)
+      // resolve(result.recordset)
 
-    // Cerrar la conexión a la base de datos
-    await sql.close()
-  } catch (err) {
-    // Manejar errores
-    console.error('Error:', err.message)
-  }
+      // Cerrar la conexión a la base de datos
+      await sql.close()
+    } catch (err) {
+      // Manejar errores
+      console.error('Error:', err.message)
+      reject(err.message)
+    }
+  })
+{
 }
 
 // Llamar a la función para ejecutar la consulta
-module.exports = execute
+module.exports = { execute }
